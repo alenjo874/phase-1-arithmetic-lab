381db9fd3f2f83ea1e333a2d7db48f0c3d570021
Alen Jo <alenjo874@gmail.com>
2022-01-09 16:23:38 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 1;
+let num2 = 62;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.ceil(Math.random() * 10);
+
+let num3 = 9;
+let num4 = 5;
+let mod = num3 % num4;
+
+let max = Math.max(1, 20);
